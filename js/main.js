e071d73dcb7a0eafdba6e6b8e2cd6a59b782f0ee
bitifyChen <43808882+bitifyChen@users.noreply.github.com>
2022-11-03 22:11:18 +0800

    221101-index ING

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+window.addEventListener("scroll", function () {
+  enterCheck(); //fade active
+  scrollBgc(); // background color change
+});
+
 // Navbar
 const headerCheck = document.querySelector("#header-check");
 
@@ -19,48 +24,86 @@ function scrollFunction() {
     }
   }
 }
+// Item active
+const onceItems = document.querySelectorAll(".fadeOnce");
+const items = document.querySelectorAll(".fade");
+
+function enterCheck() {
+  const windowHigh = window.innerHeight;
+  onceItems.forEach((item) => {
+    const toTopHight = item.getBoundingClientRect().y;
+    let fadeDelay = 0;
+    if (toTopHight < windowHigh / 1.25) {
+      if (item.getAttribute("data-fade-delay")) {
+        fadeDelay = item.getAttribute("data-fade-delay");
+      }
+      setTimeout(function () {
+        item.classList.add("active");
+      }, fadeDelay);
+    }
+  });
+
+  items.forEach((item) => {
+    const toTopHight = item.getBoundingClientRect().y;
+    let fadeDelay = 0;
+    if (toTopHight < windowHigh / 1.25) {
+      if (item.getAttribute("data-fade-delay")) {
+        fadeDelay = item.getAttribute("data-fade-delay");
+      }
+      setTimeout(function () {
+        item.classList.add("active");
+      }, fadeDelay);
+    }
+    else {
+      item.classList.remove("active");
+    }
+  });
+
+}
+enterCheck();
+
 
 // Drag items (Need CSS)
-document.addEventListener('DOMContentLoaded', ()=> {
-  const ele = document.querySelector('.drag-scroll');
-  ele.style.cursor = 'grab';
+document.addEventListener("DOMContentLoaded", () => {
+  const ele = document.querySelector(".drag-scroll");
+  ele.style.cursor = "grab";
 
   let pos = { top: 0, left: 0, x: 0, y: 0 };
 
   const mouseDownHandler = function (e) {
-      ele.style.cursor = 'grabbing';
-      ele.style.userSelect = 'none';
+    ele.style.cursor = "grabbing";
+    ele.style.userSelect = "none";
 
-      pos = {
-          left: ele.scrollLeft,
-          top: ele.scrollTop,
-          // Get the current mouse position
-          x: e.clientX,
-          y: e.clientY,
-      };
+    pos = {
+      left: ele.scrollLeft,
+      top: ele.scrollTop,
+      // Get the current mouse position
+      x: e.clientX,
+      y: e.clientY,
+    };
 
-      document.addEventListener('mousemove', mouseMoveHandler);
-      document.addEventListener('mouseup', mouseUpHandler);
+    document.addEventListener("mousemove", mouseMoveHandler);
+    document.addEventListener("mouseup", mouseUpHandler);
   };
 
   const mouseMoveHandler = function (e) {
-      // How far the mouse has been moved
-      const dx = e.clientX - pos.x;
-      const dy = e.clientY - pos.y;
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+    const dy = e.clientY - pos.y;
 
-      // Scroll the element
-      ele.scrollTop = pos.top - dy;
-      ele.scrollLeft = pos.left - dx;
+    // Scroll the element
+    ele.scrollTop = pos.top - dy;
+    ele.scrollLeft = pos.left - dx;
   };
 
   const mouseUpHandler = function () {
-      ele.style.cursor = 'grab';
-      ele.style.removeProperty('user-select');
+    ele.style.cursor = "grab";
+    ele.style.removeProperty("user-select");
 
-      document.removeEventListener('mousemove', mouseMoveHandler);
-      document.removeEventListener('mouseup', mouseUpHandler);
+    document.removeEventListener("mousemove", mouseMoveHandler);
+    document.removeEventListener("mouseup", mouseUpHandler);
   };
 
   // Attach the handler
-  ele.addEventListener('mousedown', mouseDownHandler);
+  ele.addEventListener("mousedown", mouseDownHandler);
 });
